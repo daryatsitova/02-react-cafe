e02d14c78dea479f88d057565a7ce3725d0912b4
citauka <daryaatsitova@icloud.com>
2025-11-05 23:16:53 +0100

    step 3

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,40 @@
 import { useState } from 'react'
 import css from './App.module.css'
-
+import type { VoteType, Votes } from '../../types/votes'
 
 import CafeInfo from '../CafeInfo/CafeInfo'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [votes, setVote] = useState<Votes>({ good: 0, neutral: 0, bad: 0 });
+
+
+   const updateVotes = (key: keyof Votes) => {
+    setVote({
+      ...votes,
+      [key]: votes[key] + 1,
+    });
+  };
+
+   const handleVote = (type: VoteType): void => {
+    updateVotes(type);
+  };
+
+  const resetVotes = (): void => {
+    setVote({
+      ...votes,
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
 
   return (
     <>
-      <div className={css.app}></div>
+      <div className={css.app}>
+        <CafeInfo />
+      </div>
+     
     </>
   )
 }
 
-export default App
