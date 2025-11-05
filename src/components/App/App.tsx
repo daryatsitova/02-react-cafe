bbc408ffed9adfa46b914b5315f5ce6a327fcf57
citauka <daryaatsitova@icloud.com>
2025-11-05 22:54:04 +0100

    App component initial

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import css from './App.module.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className={css.app}></div>
+    </>
+  )
+}
+
+export default App
